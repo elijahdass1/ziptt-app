df6e8145203bdf61d1838c497786b390b65d36ac
elijahdass1 <elijah.dass1@gmail.com>
2026-04-12 04:44:44 -0400

    fix: dynamic export app/(storefront)/products/page.tsx

--- app/(storefront)/products/page.tsx
+++ app/(storefront)/products/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic'
 import { Suspense } from 'react'
 import { ProductFilters } from '@/components/storefront/ProductFilters'
 import { ProductGrid } from '@/components/storefront/ProductGrid'
