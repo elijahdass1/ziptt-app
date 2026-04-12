1395a448a8f9f316106fcc568fde3cd6570f1c36
elijahdass1 <elijah.dass1@gmail.com>
2026-04-12 04:44:58 -0400

    fix: dynamic export app/admin/reviews/page.tsx

--- app/admin/reviews/page.tsx
+++ app/admin/reviews/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic'
 import prisma from '@/lib/prisma'
 import { Star } from 'lucide-react'
 import { AdminReviewActions } from '@/components/admin/AdminReviewActions'
